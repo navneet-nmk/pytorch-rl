706e12f01c96783ca21241fd00c4bbe001682f23
Navneet Madhu Kumar <navneet.nmk@gmail.com>
2018-08-08 13:31:27 +0200

    Added spectral norm

---
+++ Layers/Spectral_norm.py
@@ -0,0 +1,77 @@
+"""
+
+Implementation of the spectral normalization for the weight matrix
+
+"""
+
+import torch
+import torch.nn as nn
+from torch.nn import Parameter
+
+
+class SpectralNorm(nn.Module):
+
+    def __init__(self, module, name_module='weight', power_iterations=1):
+        super(SpectralNorm, self).__init__()
+        self.module= module
+        self.name = name_module
+        self.power_iter = power_iterations
+
+        if not self._made_params():
+            self._make_params()
+
+
+    def update_u_v(self):
+        """
+        Updating the parameters u and v which would be used for the
+        calculation of the spectral norm.
+        :return:
+        """
+
+        u = getattr(self.module, self.name + "_u")
+        v = getattr(self.module, self.name + "_v")
+        w = getattr(self.module, self.name + "_bar")
+
+        height = w.data.shape[0]
+        for _ in range(self.power_iterations):
+            v.data = self.l2normalize(torch.mv(torch.t(w.view(height, -1).data), u.data))
+            u.data = self.l2normalize(torch.mv(w.view(height, -1).data, v.data))
+
+        # sigma = torch.dot(u.data, torch.mv(w.view(height,-1).data, v.data))
+        sigma = u.dot(w.view(height, -1).mv(v))
+
+        setattr(self.module, self.name, w / sigma.expand_as(w))
+
+
+    def l2normalize(self, v, eps=1e-12):
+        return v / (v.norm() + eps)
+
+    def _made_params(self):
+        try:
+            u = getattr(self.module, self.name + "_u")
+            v = getattr(self.module, self.name + "_v")
+            w = getattr(self.module, self.name + "_bar")
+            return True
+        except AttributeError:
+            return False
+
+    def _make_params(self):
+        w = getattr(self.module, self.name)
+
+        height = w.data.shape[0]
+        width = w.view(height, -1).data.shape[1]
+
+        u = Parameter(w.data.new(height).normal_(0, 1), requires_grad=False)
+        v = Parameter(w.data.new(width).normal_(0, 1), requires_grad=False)
+        u.data = self.l2normalize(u.data)
+        v.data = self.l2normalize(v.data)
+        w_bar = Parameter(w.data)
+
+        del self.module._parameters[self.name]
+        self.module.register_parameter(self.name + "_u", u)
+        self.module.register_parameter(self.name + "_v", v)
+        self.module.register_parameter(self.name + "_bar", w_bar)
+
+    def forward(self, *args):
+        self.update_u_v()
+        return self.module.forward(*args)
