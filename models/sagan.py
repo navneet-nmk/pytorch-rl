d8a5e9b5dc7116f72a2078b84d75a2e69479edaf
Navneet Madhu Kumar <navneet.nmk@gmail.com>
2018-08-10 10:33:12 +0200

    Added training script

--- models/sagan.py
+++ models/sagan.py
@@ -8,12 +8,12 @@ https://arxiv.org/abs/1805.08318
 
 import torch
 import torch.nn as nn
-import torch.nn.functional as F
 from torch.optim import Adam
 from torch.utils.data import DataLoader
 
 import numpy as np
 from torch.autograd import Variable
+from torchvision.utils import save_image
 
 USE_CUDA = torch.cuda.is_available()
 
@@ -24,7 +24,8 @@ class SAGAN(object):
                  dataset, num_epochs,
                  random_seed, shuffle, use_cuda,
                  tensorboard_summary_writer,
-                 output_folder,
+                 output_folder, image_size,
+                 image_channels, noise_dim,
                  generator_lr, discriminator_lr, batch_size):
 
 
@@ -42,6 +43,9 @@ class SAGAN(object):
         self.discriminator = discriminator
         self.tb_writer = tensorboard_summary_writer
         self.output_folder = output_folder
+        self.image_size = image_size
+        self.img_channels = image_channels
+        self.noise_dim = noise_dim
 
         if self.use_cuda:
             self.generator = self.generator.cuda()
@@ -63,7 +67,6 @@ class SAGAN(object):
                                     shuffle=self.shuffle)
 
         return dataset_loader
-
 
     # Discriminator Loss function
     def disc_loss(self):
@@ -88,6 +91,106 @@ class SAGAN(object):
 
     # The main training loop function
     def train(self):
-        pass
+        real_x = torch.FloatTensor(self.batch_size, self.img_channels,
+                                   self.image_size, self.image_size)
+        labels = torch.FloatTensor(self.batch_size)
+        noise = torch.FloatTensor(self.batch_size, self.noise_dim)
 
+        noise = Variable(noise)
+        labels = Variable(labels)
+        labels.requires_grad = False
 
+        criterionD = self.disc_loss()
+
+        # For inference
+        fix_noise = torch.Tensor(self.noise_dim).uniform_(-1, 1)
+
+        # Main training loop
+        for epoch in range(self.num_epochs):
+            std = 1.0
+            for num_iters, batch_data in enumerate(self.get_dataloader()):
+                # Real Part
+                self.dis_optim.zero_grad()
+
+                x = batch_data['image']
+                bs = x.size(0)
+
+                x = Variable(x)
+
+                if self.use_cuda:
+                    x = x.cuda()
+                    real_x = real_x.cuda()
+                    noise = noise.cuda()
+                    labels = labels.cuda()
+
+                real_x.data.copy_(x)
+                # Add noise to the inputs of the discriminator
+
+                # This is an auxillary noise added to the discriminator to stabilize the training
+                noise_data = torch.zeros(x.shape)
+                #            print(noise.shape)
+                noise_data = torch.normal(mean=noise_data, std=std)
+                if self.use_cuda:
+                    noise_data = noise_data.cuda()
+
+                x += noise_data
+                d_output = self.discriminator(x)
+                labels.data.fill_(1)
+                loss_real = criterionD(d_output, labels)
+                loss_real.backward()
+
+                # Fake Part
+                z = self._noise_sample(noise, bs)
+                fake_x = self.generator(z)
+
+                fake_x = fake_x + noise_data
+                d_output = self.discriminator(fake_x.detach())
+                labels.data.fill_(0)
+                loss_fake = criterionD(d_output, labels)
+                loss_fake.backward()
+
+                D_loss = loss_real + loss_fake
+                self.dis_optim.step()
+
+                # Generator Loss update
+                d_output = self.discriminator(fake_x)
+                labels.data.fill_(1.0)
+                reconstruct_loss = criterionD(d_output, labels)
+
+                self.gen_optim.zero_grad()
+
+                G_loss = reconstruct_loss
+                G_loss.backward()
+
+                self.gen_optim.step()
+
+                if num_iters % 100 == 0:
+                    print('Epoch/Iter:{0}/{1}, Dloss: {2}, Gloss: {3}'.format(
+                        epoch, num_iters, D_loss.data.cpu().numpy(),
+                        G_loss.data.cpu().numpy())
+                    )
+
+                    std = self.linear_annealing_variance(std=std, epoch=epoch)
+
+                    noise.data.copy_(fix_noise)
+
+                    z = noise
+                    x_save = self.generator(z)
+                    save_image(x_save.data.cpu(), 'infogan/inference/'+ str(epoch)+'c1.png', nrow=8)
+
+            self.save_model(output=self.output_folder)
+
+    def to_cuda(self):
+        self.generator = self.generator.cuda()
+        self.discriminator = self.discriminator.cuda()
+
+    def save_model(self, output):
+        print("Saving the generator and discriminator")
+        torch.save(
+                self.generator.state_dict(),
+                '{}/generator.pt'.format(output)
+            )
+        torch.save(
+                    self.discriminator.state_dict(),
+                    '{}/discriminator.pt'.format(output)
+                )
