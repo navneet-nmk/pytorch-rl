20ec48773cff2e5d749ae973bf4a5287d973a48a
Navneet Madhu Kumar <navneet.nmk@gmail.com>
2018-08-12 22:36:05 +0200

    Added the critic:

--- models/SAC.py
+++ models/SAC.py
@@ -7,7 +7,7 @@ the current policy added to the reward.
 
 The maximization of the augmented reward enables the agent to discover multimodal actions
 (Multiple actions that results in reward). It promotes exploration
-and is consoderably more stable to random seeds variation and hyperparameter
+and is considerably more stable to random seeds variation and hyperparameter
 tuning compared to Deep Deterministic Policy Gradient.
 
 """
@@ -17,23 +17,63 @@ import torch.nn as nn
 
 USE_CUDA = torch.cuda.is_available()
 
-class SAC(nn.Module):
+
+class SAC(object):
 
     def __init__(self):
-        super(SAC, self).__init__()
+        pass
 
 
-
-
+# The Policy Network
 class StochasticActor(nn.Module):
 
     def __init__(self):
         super(StochasticActor, self).__init__()
 
 
+# Estimates the Q value of a state action pair
 class Critic(nn.Module):
 
-    def __init__(self):
+    def __init__(self, state_dim, action_dim,
+                 hidden_dim, output_dim):
         super(Critic, self).__init__()
 
-        
+        self.state_dim = state_dim
+        self.action_dim = action_dim
+        self.hidden = hidden_dim
+        self.output_dim = output_dim
+
+        # Architecture
+        self.input = nn.Linear(in_features=self.state_dim+self.action_dim, out_features=self.hidden)
+        self.hidden_1 = nn.Linear(in_features=self.hidden, out_features=self.hidden*2)
+        self.hidden_2 = nn.Linear(in_features=self.hidden*2, out_features=self.hidden*2)
+        self.output = nn.Linear(in_features=self.hidden*2, out_features=self.output_dim)
+
+        # Leaky Relu activation
+        self.lrelu = nn.LeakyReLU()
+
+        # Initialize the weights with xavier initialization
+        nn.init.xavier_uniform_(self.input.weight)
+        nn.init.xavier_uniform_(self.hidden_1.weight)
+        nn.init.xavier_uniform_(self.hidden_2.weight)
+        nn.init.xavier_uniform_(self.output.weight)
+
+    def forward(self, x):
+        x = self.input(x)
+        x = self.lrelu(x)
+        x = self.hidden_1(x)
+        x = self.lrelu(x)
+        x = self.hidden_2(x)
+        x = self.lrelu(x)
+        output = self.output(x)
+        return output
+
+
+
+# Estimates the value of a state
+class ValueNetwork(nn.Module):
+
+
+    def __init__(self):
+        super(ValueNetwork, self).__init__()
+
