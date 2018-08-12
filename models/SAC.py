d9e2bbe1d6664cf2d134cf94f57b88ec171addd2
Navneet Madhu Kumar <navneet.nmk@gmail.com>
2018-08-12 23:06:39 +0200

    Added the value network

--- models/SAC.py
+++ models/SAC.py
@@ -14,6 +14,7 @@ tuning compared to Deep Deterministic Policy Gradient.
 
 import torch
 import torch.nn as nn
+from torch.autograd import Variable
 
 USE_CUDA = torch.cuda.is_available()
 
@@ -27,8 +28,64 @@ class SAC(object):
 # The Policy Network
 class StochasticActor(nn.Module):
 
-    def __init__(self):
+    def __init__(self, state_dim, action_dim,
+                 hidden_dim, use_tanh=False):
         super(StochasticActor, self).__init__()
+
+        self.state_dim = state_dim
+        self.action_dim = action_dim
+        self.hidden = hidden_dim
+        self.use_tanh = use_tanh
+
+        # Architecture
+        self.input = nn.Linear(in_features=self.state_dim, out_features=self.hidden)
+        self.hidden_1 = nn.Linear(in_features=self.hidden, out_features=self.hidden*2)
+        self.hidden_2 = nn.Linear(in_features=self.hidden*2, out_features=self.hidden*2)
+        self.output_mu = nn.Linear(in_features=self.hidden*2, out_features=self.action_dim)
+        self.output_logvar = nn.Linear(in_features=self.hidden*2, out_features=self.action_dim)
+
+        # Leaky Relu activation function
+        self.lrelu = nn.LeakyReLU()
+
+        #Output Activation function
+        self.tanh = nn.Tanh()
+
+        # Initialize the weights with xavier initialization
+        nn.init.xavier_uniform_(self.input.weight)
+        nn.init.xavier_uniform_(self.hidden_1.weight)
+        nn.init.xavier_uniform_(self.hidden_2.weight)
+        nn.init.xavier_uniform_(self.output.weight)
+
+    def reparameterize(self, mu, logvar):
+        # Reparameterization trick as shown in the auto encoding variational bayes paper
+        if self.training:
+            std = logvar.mul(0.5).exp_()
+            eps = Variable(std.data.new(std.size()).normal_())
+            if self.use_cuda:
+                eps = eps.cuda()
+            return eps.mul(std).add_(mu)
+        else:
+            return mu
+
+    def forward(self, state):
+        x = self.input(state)
+        x = self.input(x)
+        x = self.lrelu(x)
+        x = self.hidden_1(x)
+        x = self.lrelu(x)
+        x = self.hidden_2(x)
+        x = self.lrelu(x)
+
+        mu = self.output_mu(x)
+        logvar = self.output_logvar(x)
+
+        output = self.reparameterize(mu, logvar)
+
+        if self.use_tanh:
+            output = self.tanh(output)
+
+        return output, mu, logvar
+
 
 
 # Estimates the Q value of a state action pair
@@ -82,7 +139,7 @@ class ValueNetwork(nn.Module):
         self.output_dim = output_dim
 
         # Architecture
-        self.input = nn.Linear(in_features=self.state_dim + self.action_dim, out_features=self.hidden)
+        self.input = nn.Linear(in_features=self.state_dim, out_features=self.hidden)
         self.hidden_1 = nn.Linear(in_features=self.hidden, out_features=self.hidden * 2)
         self.hidden_2 = nn.Linear(in_features=self.hidden * 2, out_features=self.hidden * 2)
         self.output = nn.Linear(in_features=self.hidden * 2, out_features=self.output_dim)
