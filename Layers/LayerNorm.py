b2a50db3a6ad4f0dd6eb1016d3229b6855872c81
Navneet Madhu Kumar <navneet.nmk@gmail.com>
2018-08-14 09:28:55 +0200

    Added layer norm

---
+++ Layers/LayerNorm.py
@@ -0,0 +1,15 @@
+import torch
+import torch.nn as nn
+
+
+class LayerNorm(nn.Module):
+    def __init__(self, features, eps=1e-6):
+        super().__init__()
+        self.gamma = nn.Parameter(torch.ones(features))
+        self.beta = nn.Parameter(torch.zeros(features))
+        self.eps = eps
+
+    def forward(self, x):
+        mean = x.mean(-1, keepdim=True)
+        std = x.std(-1, keepdim=True)
+        return self.gamma * (x - mean) / (std + self.eps) + self.beta
