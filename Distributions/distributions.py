38a6213abf2ba9bf7e9a0e6f6ccce67a4c68ae35
Navneet Madhu Kumar <navneet.nmk@gmail.com>
2018-08-14 09:42:01 +0200

    Added tanh distribution

--- Distributions/distributions.py
+++ Distributions/distributions.py
@@ -39,50 +39,99 @@ except ImportError:
             raise NotImplementedError
 
 
-    # Sigmoid Normal Distribution
-    class SigmoidNormal(Distribution):
-        """
+# Sigmoid Normal Distribution
+class SigmoidNormal(Distribution):
+    """
         Represent distribution of X where
             X ~ sigmoid(Z)
             Z ~ N(mean, std)
         Note: this is not very numerically stable.
         """
 
-        def __init__(self, normal_mean, normal_std, epsilon=1e-6):
-            """
-            :param normal_mean: Mean of the normal distribution
-            :param normal_std: Std of the normal distribution
-            :param epsilon: Numerical stability epsilon when computing log-prob.
-            """
-            self.normal = Normal(normal_mean, normal_std)
-            self.epsilon = epsilon
+    def __init__(self, normal_mean, normal_std, epsilon=1e-6):
+        super(SigmoidNormal, self).__init__()
+        """
+        :param normal_mean: Mean of the normal distribution
+        :param normal_std: Std of the normal distribution
+        :param epsilon: Numerical stability epsilon when computing log-prob.
+        """
+        self.normal = Normal(normal_mean, normal_std)
+        self.epsilon = epsilon
 
-        def sample_n(self, n, return_pre_sigmoid_value=False):
-            z = self.normal.sample_n(n)
-            if return_pre_sigmoid_value:
-                return F.sigmoid(z), z
-            else:
-                return F.sigmoid(z)
+    def sample_n(self, n, return_pre_sigmoid_value=False):
+        z = self.normal.sample_n(n)
+        if return_pre_sigmoid_value:
+            return F.sigmoid(z), z
+        else:
+            return F.sigmoid(z)
 
-        def log_prob(self, value, pre_sigmoid_value=None):
-            """
-            :param value: some value, x
-            :param pre_sigmoid_value: arcsigmoid(x)
-            :return:
-            """
-            if pre_sigmoid_value is None:
-                pre_sigmoid_value = torch.log(
+    def log_prob(self, value, pre_sigmoid_value=None):
+        """
+        :param value: some value, x
+        :param pre_sigmoid_value: arcsigmoid(x)
+        :return:
+        """
+        if pre_sigmoid_value is None:
+            pre_sigmoid_value = torch.log(
                     (value) / (1 - value)
                 )
-            return self.normal.log_prob(pre_sigmoid_value) - torch.log(
+        return self.normal.log_prob(pre_sigmoid_value) - torch.log(
                 value*(1-value) + self.epsilon
-            )
+        )
 
-        def sample(self, return_pre_sigmoid_value=False):
-            z = self.normal.sample()
-            if return_pre_sigmoid_value:
-                return F.sigmoid(z), z
-            else:
-                return F.sigmoid(z)
+    def sample(self, return_pre_sigmoid_value=False):
+        z = self.normal.sample()
+        if return_pre_sigmoid_value:
+            return F.sigmoid(z), z
+        else:
+            return F.sigmoid(z)
+
+
+# Tanh Normal Distribution
+class TanhNormal(Distribution):
+    """
+        Represent distribution of X where
+            X ~ tanh(Z)
+            Z ~ N(mean, std)
+        Note: this is not very numerically stable.
+        """
+
+    def __init__(self, normal_mean, normal_std, epsilon=1e-6):
+        super(TanhNormal, self).__init__()
+        """
+        :param normal_mean: Mean of the normal distribution
+        :param normal_std: Std of the normal distribution
+        :param epsilon: Numerical stability epsilon when computing log-prob.
+        """
+        self.normal = Normal(normal_mean, normal_std)
+        self.epsilon = epsilon
+
+    def sample_n(self, n, return_pre_tanh_value=False):
+        z = self.normal.sample_n(n)
+        if return_pre_tanh_value:
+            return F.tanh(z), z
+        else:
+            return F.tanh(z)
+
+    def log_prob(self, value, pre_tanh_value=None):
+        """
+        :param value: some value, x
+        :param pre_sigmoid_value: arcsigmoid(x)
+        :return:
+        """
+        if pre_tanh_value is None:
+            pre_sigmoid_value = torch.log(
+                    (1+value) / (1 - value)
+                )/2
+        return self.normal.log_prob(pre_tanh_value) - torch.log(
+                1- value*value + self.epsilon
+        )
+
+    def sample(self, return_pre_tanh_value=False):
+        z = self.normal.sample()
+        if return_pre_tanh_value:
+            return F.tanh(z), z
+        else:
+            return F.tanh(z)
 
 
