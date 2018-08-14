a47843a62f7a87dfd5395db1166a6b7c3ceef205
Navneet Madhu Kumar <navneet.nmk@gmail.com>
2018-08-14 17:16:45 +0200

    Added adaptive param noise

--- Utils/random_process.py
+++ Utils/random_process.py
@@ -48,3 +48,32 @@ class OrnsteinUhlenbeckActionNoise:
         dx = dx + self.sigma * np.random.randn(len(self.X))
         self.X = self.X + dx
         return self.X
+
+
+# Adaptive param noise for parameter perturbations
+class AdaptiveParamNoise(object):
+
+    def __init__(self, initial_stddev=0.1, desired_action_stddev=0.1, adoption_coefficient=1.01):
+        self.initial_stddev = initial_stddev
+        self.desired_action_stddev = desired_action_stddev
+        self.adoption_coefficient = adoption_coefficient
+
+        self.current_stddev = initial_stddev
+
+    def adapt(self, distance):
+        if distance > self.desired_action_stddev:
+            # Decrease stddev.
+            self.current_stddev /= self.adoption_coefficient
+        else:
+            # Increase stddev.
+            self.current_stddev *= self.adoption_coefficient
+
+    def get_stats(self):
+        stats = {
+            'param_noise_stddev': self.current_stddev,
+        }
+        return stats
+
+    def __repr__(self):
+        fmt = 'AdaptiveParamNoiseSpec(initial_stddev={}, desired_action_stddev={}, adoption_coefficient={})'
+        return fmt.format(self.initial_stddev, self.desired_action_stddev, self.adoption_coefficient)
