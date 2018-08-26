e74af0d6aea8e1b7c369c64d1af8ac408f910fe7
Navneet Madhu Kumar <navneet.nmk@gmail.com>
2018-08-26 22:57:46 +0200

    Added inverse dynamics distribution

--- models/empowerment_models.py
+++ models/empowerment_models.py
@@ -73,5 +73,75 @@ class source_distribution(nn.Module):
         output = self.output_activ(x)
 
         return output
-    
 
+class inverse_dynamics_distribution(nn.Module):
+    
+    def __init__(self, state_space,
+                 action_space,
+                 height, width,
+                 conv_kernel_size,
+                 conv_layers, hidden):
+        super(inverse_dynamics_distribution, self).__init__()
+        self.state_space = state_space
+        self.action_space = action_space
+        self.height = height
+        self.width = width
+        self.conv_kernel_size = conv_kernel_size
+        self.hidden = hidden
+        self.conv_layers = conv_layers
+
+        # Inverse Dynamics Architecture
+
+        # Given the current state and the next state, this network predicts the action
+
+        self.conv1 = nn.Conv2d(in_channels=self.input_channels*2,
+                               out_channels=self.conv_layers,
+                               kernel_size=self.conv_kernel_size, stride=2)
+        self.conv2 = nn.Conv2d(in_channels=self.conv_layers,
+                               out_channels=self.conv_layers,
+                               kernel_size=self.conv_kernel_size, stride=2)
+        self.conv3 = nn.Conv2d(in_channels=self.conv_layers,
+                               out_channels=self.conv_layers * 2,
+                               kernel_size=self.conv_kernel_size, stride=2)
+        self.conv4 = nn.Conv2d(in_channels=self.conv_layers * 2,
+                               out_channels=self.conv_layers * 2,
+                               kernel_size=self.conv_kernel_size, stride=2)
+
+        # Leaky relu activation
+        self.lrelu = nn.LeakyReLU(inplace=True)
+
+        # Hidden Layers
+        self.hidden_1 = nn.Linear(in_features=self.height // 16 * self.width // 16 * self.conv_layers * 2,
+                                  out_features=self.hidden)
+        self.output = nn.Linear(in_features=self.hidden, out_features=self.action_space)
+
+        # Output activation function
+        self.output_activ = nn.Softmax()
+
+    def forward(self, current_state, next_state):
+        state = torch.cat([current_state, next_state], dim=-1)
+        x = self.conv1(state)
+        x = self.lrelu(x)
+        x = self.conv2(x)
+        x = self.lrelu(x)
+        x = self.conv3(x)
+        x = self.lrelu(x)
+        x = self.conv4(x)
+        x = self.lrelu(x)
+
+        x = x.view((-1, self.height // 16 * self.width // 16 * self.conv_layers * 2))
+        x = self.hidden_1(x)
+        x = self.lrelu(x)
+        x = self.output(x)
+        output = self.output_activ(x)
+
+        return output
+
+
+class forward_dynamics_model(nn.Module):
+
+    def __init__(self, height,
+                 width,
+                 state_space, action_space,
+                 conv_kernel_size, conv_layers):
+        super(forward_dynamics_model, self).__init__()
