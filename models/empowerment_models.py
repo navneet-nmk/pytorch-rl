43035fcab03b08074b46b938fcd80efd0fb1c72b
Navneet Madhu Kumar <navneet.nmk@gmail.com>
2018-08-27 10:54:17 +0200

    Added forward dynamics model

--- models/empowerment_models.py
+++ models/empowerment_models.py
@@ -168,10 +168,17 @@ class inverse_dynamics_distribution(nn.Module):
         self.conv_kernel_size = conv_kernel_size
         self.hidden = hidden
         self.conv_layers = conv_layers
+        self.use_encoding = use_encoding
 
         # Inverse Dynamics Architecture
 
         # Given the current state and the next state, this network predicts the action
+
+        self.layer1 = nn.Linear(in_features=self.state_space*2, out_features=self.hidden)
+        self.layer2 = nn.Linear(in_features=self.hidden, out_features=self.hidden)
+        self.layer3 = nn.Linear(in_features=self.hidden, out_features=self.hidden * 2)
+        self.layer4 = nn.Linear(in_features=self.hidden * 2, out_features=self.hidden * 2)
+        self.hidden_1 = nn.Linear(in_features=self.hidden * 2, out_features=self.hidden)
 
         self.conv1 = nn.Conv2d(in_channels=self.input_channels*2,
                                out_channels=self.conv_layers,
@@ -199,17 +206,28 @@ class inverse_dynamics_distribution(nn.Module):
 
     def forward(self, current_state, next_state):
         state = torch.cat([current_state, next_state], dim=-1)
-        x = self.conv1(state)
-        x = self.lrelu(x)
-        x = self.conv2(x)
-        x = self.lrelu(x)
-        x = self.conv3(x)
-        x = self.lrelu(x)
-        x = self.conv4(x)
-        x = self.lrelu(x)
+        if self.use_encoding:
+            x = self.layer1(state)
+            x = self.lrelu(x)
+            x = self.layer2(x)
+            x = self.lrelu(x)
+            x = self.layer3(x)
+            x = self.lrelu(x)
+            x = self.layer4(x)
+            x = self.lrelu(x)
+            x = self.hidden_1(x)
+        else:
+            x = self.conv1(state)
+            x = self.lrelu(x)
+            x = self.conv2(x)
+            x = self.lrelu(x)
+            x = self.conv3(x)
+            x = self.lrelu(x)
+            x = self.conv4(x)
+            x = self.lrelu(x)
+            x = x.view((-1, self.height // 16 * self.width // 16 * self.conv_layers * 2))
+            x = self.hidden_1(x)
 
-        x = x.view((-1, self.height // 16 * self.width // 16 * self.conv_layers * 2))
-        x = self.hidden_1(x)
         x = self.lrelu(x)
         x = self.output(x)
         output = self.output_activ(x)
@@ -222,7 +240,9 @@ class forward_dynamics_model(nn.Module):
     def __init__(self, height,
                  width,
                  state_space, action_space,
-                 conv_kernel_size, conv_layers, hidden):
+                 input_channels, conv_kernel_size,
+                 conv_layers, hidden,
+                 use_encoding=True):
         super(forward_dynamics_model, self).__init__()
         self.state_space = state_space
         self.action_space = action_space
@@ -231,46 +251,40 @@ class forward_dynamics_model(nn.Module):
         self.conv_kernel_size = conv_kernel_size
         self.hidden = hidden
         self.conv_layers = conv_layers
+        self.use_encoding = use_encoding
+        self.input_channels = input_channels
 
         # Forward Dynamics Model Architecture
 
         # Given the current state and the action, this network predicts the next state
 
-        self.conv1 = nn.Conv2d(in_channels=self.input_channels,
-                               out_channels=self.conv_layers,
-                               kernel_size=self.conv_kernel_size, stride=2)
-        self.conv2 = nn.Conv2d(in_channels=self.conv_layers,
-                               out_channels=self.conv_layers,
-                               kernel_size=self.conv_kernel_size, stride=2)
-        self.conv3 = nn.Conv2d(in_channels=self.conv_layers,
-                               out_channels=self.conv_layers * 2,
-                               kernel_size=self.conv_kernel_size, stride=2)
-        self.conv4 = nn.Conv2d(in_channels=self.conv_layers * 2,
-                               out_channels=self.conv_layers * 2,
-                               kernel_size=self.conv_kernel_size, stride=2)
-
-        # Leaky relu activation
-        self.lrelu = nn.LeakyReLU(inplace=True)
-
-        # Hidden Layers
-        self.hidden_1 = nn.Linear(in_features=self.height // 16 * self.width // 16 * self.conv_layers * 2,
+        self.layer1 = nn.Linear(in_features=self.state_space * 2, out_features=self.hidden)
+        self.layer2 = nn.Linear(in_features=self.hidden, out_features=self.hidden)
+        self.layer3 = nn.Linear(in_features=self.hidden, out_features=self.hidden * 2)
+        self.layer4 = nn.Linear(in_features=self.hidden * 2, out_features=self.hidden * 2)
+        self.hidden_1 = nn.Linear(in_features=self.hidden*2,
                                   out_features=self.hidden)
         self.output = nn.Linear(in_features=self.hidden+self.action_space, out_features=self.state_space)
 
     def forward(self, current_state, action):
-        x = self.conv1(current_state)
+        x = self.layer1(current_state)
         x = self.lrelu(x)
-        x = self.conv2(x)
+        x = self.layer2(x)
         x = self.lrelu(x)
-        x = self.conv3(x)
+        x = self.layer3(x)
         x = self.lrelu(x)
-        x = self.conv4(x)
+        x = self.layer4(x)
         x = self.lrelu(x)
-
-        x = x.view((-1, self.height // 16 * self.width // 16 * self.conv_layers * 2))
         x = self.hidden_1(x)
         x = self.lrelu(x)
         x = torch.cat([x, action], dim=-1)
         output = self.output(x)
 
         return output
+
+
+
+class EmpowermentTrainer(object):
+
+    def __init__(self):
+        pass
