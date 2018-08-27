5cf7e412abb138ef8bd4d484ff3643b09355ead8
Navneet Madhu Kumar <navneet.nmk@gmail.com>
2018-08-27 10:40:34 +0200

    Use state encoding

--- models/empowerment_models.py
+++ models/empowerment_models.py
@@ -79,11 +79,13 @@ class Encoder(nn.Module):
 class source_distribution(nn.Module):
 
     def __init__(self,
-                 state_space,
                  action_space,
                  conv_kernel_size,
                  conv_layers,
-                 hidden, input_channels, height, width):
+                 hidden, input_channels,
+                 height, width,
+                 state_space=None,
+                 use_encoded_state=True):
         super(source_distribution, self).__init__()
 
         self.state_space = state_space
@@ -94,45 +96,55 @@ class source_distribution(nn.Module):
         self.conv_layers = conv_layers
         self.height = height
         self.width = width
+        self.use_encoding = use_encoded_state
 
         # Source Architecture
         # Given a state, this network predicts the action
 
-        self.conv1 = nn.Conv2d(in_channels=self.input_channels,
-                               out_channels=self.conv_layers,
-                               kernel_size=self.conv_kernel_size, stride=2)
-        self.conv2 = nn.Conv2d(in_channels=self.conv_layers,
-                               out_channels=self.conv_layers,
-                               kernel_size=self.conv_kernel_size, stride=2)
-        self.conv3 = nn.Conv2d(in_channels=self.conv_layers,
-                               out_channels=self.conv_layers*2,
-                               kernel_size=self.conv_kernel_size, stride=2)
-        self.conv4 = nn.Conv2d(in_channels=self.conv_layers*2,
-                               out_channels=self.conv_layers*2,
-                               kernel_size=self.conv_kernel_size, stride=2)
+        if use_encoded_state:
+            self.layer1 = nn.Linear(in_features=self.state_space, out_features=self.hidden)
+            self.layer2 = nn.Linear(in_features=self.hidden, out_features=self.hidden)
+            self.layer3 = nn.Linear(in_features=self.hidden, out_features=self.hidden*2)
+            self.layer4 = nn.Linear(in_features=self.hidden*2, out_features=self.hidden*2)
+            self.hidden_1 = nn.Linear(in_features=self.hidden*2, out_features=self.hidden)
+        else:
+            self.layer1 = nn.Conv2d(in_channels=self.input_channels,
+                                   out_channels=self.conv_layers,
+                                   kernel_size=self.conv_kernel_size, stride=2)
+            self.layer2 = nn.Conv2d(in_channels=self.conv_layers,
+                                   out_channels=self.conv_layers,
+                                   kernel_size=self.conv_kernel_size, stride=2)
+            self.layer3 = nn.Conv2d(in_channels=self.conv_layers,
+                                   out_channels=self.conv_layers*2,
+                                   kernel_size=self.conv_kernel_size, stride=2)
+            self.layer4 = nn.Conv2d(in_channels=self.conv_layers*2,
+                                   out_channels=self.conv_layers*2,
+                                   kernel_size=self.conv_kernel_size, stride=2)
+
+            self.hidden_1 = nn.Linear(in_features=self.height // 16 * self.width // 16 * self.conv_layers * 2,
+                                      out_features=self.hidden)
 
         # Leaky relu activation
         self.lrelu = nn.LeakyReLU(inplace=True)
 
         # Hidden Layers
-        self.hidden_1 = nn.Linear(in_features=self.height//16*self.width//16*self.conv_layers*2,
-                                  out_features=self.hidden)
+
         self.output = nn.Linear(in_features=self.hidden, out_features=self.action_space)
 
         # Output activation function
         self.output_activ = nn.Softmax()
 
     def forward(self, current_state):
-        x = self.conv1(current_state)
+        x = self.layer1(current_state)
         x = self.lrelu(x)
-        x = self.conv2(x)
+        x = self.layer2(x)
         x = self.lrelu(x)
-        x = self.conv3(x)
+        x = self.layer3(x)
         x = self.lrelu(x)
-        x = self.conv4(x)
+        x = self.layer4(x)
         x = self.lrelu(x)
-
-        x = x.view((-1, self.height//16*self.width//16*self.conv_layers*2))
+        if not self.use_encoding:
+            x = x.view((-1, self.height//16*self.width//16*self.conv_layers*2))
         x = self.hidden_1(x)
         x = self.lrelu(x)
         x = self.output(x)
@@ -146,7 +158,8 @@ class inverse_dynamics_distribution(nn.Module):
                  action_space,
                  height, width,
                  conv_kernel_size,
-                 conv_layers, hidden):
+                 conv_layers, hidden,
+                 use_encoding=True):
         super(inverse_dynamics_distribution, self).__init__()
         self.state_space = state_space
         self.action_space = action_space
