7e68418e843dd4d24eca1238c70100cd42f74c06
Navneet Madhu Kumar <navneet.nmk@gmail.com>
2018-09-20 14:33:46 +0200

    Added prioritized experience replay

--- Memory/Buffer.py
+++ Memory/Buffer.py
@@ -7,6 +7,61 @@ Transition = namedtuple('Transition',
                         ('state', 'action', 'next_state', 'reward', 'done'))
 
 
+# Segment tree data structure where parent node values are sum/max of children node values
+class SegmentTree():
+  def __init__(self, size):
+    self.index = 0
+    self.size = size
+    self.full = False  # Used to track actual capacity
+    self.sum_tree = [0] * (2 * size - 1)  # Initialise fixed size tree with all (priority) zeros
+    self.data = [None] * size  # Wrap-around cyclic buffer
+    self.max = 1  # Initial max value to return (1 = 1^ω)
+
+  # Propagates value up tree given a tree index
+  def _propagate(self, index, value):
+    parent = (index - 1) // 2
+    left, right = 2 * parent + 1, 2 * parent + 2
+    self.sum_tree[parent] = self.sum_tree[left] + self.sum_tree[right]
+    if parent != 0:
+      self._propagate(parent, value)
+
+  # Updates value given a tree index
+  def update(self, index, value):
+    self.sum_tree[index] = value  # Set new value
+    self._propagate(index, value)  # Propagate value
+    self.max = max(value, self.max)
+
+  def append(self, data, value):
+    self.data[self.index] = data  # Store data in underlying data structure
+    self.update(self.index + self.size - 1, value)  # Update tree
+    self.index = (self.index + 1) % self.size  # Update index
+    self.full = self.full or self.index == 0  # Save when capacity reached
+    self.max = max(value, self.max)
+
+  # Searches for the location of a value in sum tree
+  def _retrieve(self, index, value):
+    left, right = 2 * index + 1, 2 * index + 2
+    if left >= len(self.sum_tree):
+      return index
+    elif value <= self.sum_tree[left]:
+      return self._retrieve(left, value)
+    else:
+      return self._retrieve(right, value - self.sum_tree[left])
+
+  # Searches for a value in sum tree and returns value, data index and tree index
+  def find(self, value):
+    index = self._retrieve(0, value)  # Search for index of item from root
+    data_index = index - self.size + 1
+    return (self.sum_tree[index], data_index, index)  # Return value, data index, tree index
+
+  # Returns data given a data index
+  def get(self, data_index):
+    return self.data[data_index % self.size]
+
+  def total(self):
+    return self.sum_tree[0]
+
+
 class ReplayBufferDataset(Dataset):
     """
     Dataset implementation of the experience replay
@@ -40,10 +95,14 @@ class ReplayBufferDataset(Dataset):
 
 class ReplayBuffer(object):
 
-    def __init__(self, capacity, seed, priotirized_experience=False):
+    def __init__(self, capacity, seed,
+                 priority_weight=None, priority_exponent=None,
+                 priotirized_experience=False):
         self.capacity = capacity
         self.position = 0
         self.prioritize = priotirized_experience
+        self.priority_weight = priority_weight  # Initial importance sampling weight β, annealed to 1 over course of training
+        self.priority_exponent = priority_exponent
         if self.prioritize:
             self.memory = []
         else:
