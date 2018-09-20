70f3c11f4eadbeb7f659e04e7bade3fbcd16d348
Navneet Madhu Kumar <navneet.nmk@gmail.com>
2018-09-20 14:29:12 +0200

    Added Subprocvenv

--- Environments/env_wrappers.py
+++ Environments/env_wrappers.py
@@ -11,6 +11,7 @@ from PIL import Image
 from gym.spaces.box import Box
 import gym
 import time, sys
+from multiprocessing import Process, Pipe
 from gym import spaces
 import cv2
 cv2.ocl.setUseOpenCL(False)
@@ -46,6 +47,128 @@ class MaxAndSkipEnv(gym.Wrapper):
         obs = self.env.reset()
         self._obs_buffer.append(obs)
         return obs
+
+def flatten_lists(listoflists):
+    return [el for list_ in listoflists for el in list_]
+
+def worker(remote, env_fn_wrapper):
+    env = env_fn_wrapper.x()
+    while True:
+        cmd, data = remote.recv()
+        if cmd == 'step':
+            ob, reward, done, info = env.step(data)
+            if done:
+                ob = env.reset()
+            remote.send((ob, reward, done, info))
+        elif cmd == 'reset':
+            ob = env.reset()
+            remote.send(ob)
+        elif cmd == 'close':
+            remote.close()
+            break
+        elif cmd == 'get_spaces':
+            remote.send((env.action_space, env.observation_space))
+        elif cmd == 'get_history':
+            senv = env
+            while not hasattr(senv, 'get_history'):
+                senv = senv.env
+            remote.send(senv.get_history(data))
+        elif cmd == 'recursive_getattr':
+            remote.send(env.recursive_getattr(data))
+        elif cmd == 'decrement_starting_point':
+            env.decrement_starting_point(data)
+        else:
+            raise NotImplementedError
+        
+
+class CloudpickleWrapper(object):
+    """
+    Uses cloudpickle to serialize contents (otherwise multiprocessing tries to use pickle)
+    """
+    def __init__(self, x):
+        self.x = x
+    def __getstate__(self):
+        import cloudpickle
+        return cloudpickle.dumps(self.x)
+    def __setstate__(self, ob):
+        import pickle
+        self.x = pickle.loads(ob)
+    
+class SubprocVecEnv(gym.Wrapper):
+    def __init__(self, env_fns):
+        """
+        envs: list of gym environments to run in subprocesses
+        """
+        
+        super(SubprocVecEnv, self).__init__(env=env_fns)
+        nenvs = len(env_fns)
+        self.remotes, self.work_remotes = zip(*[Pipe() for _ in range(nenvs)])
+        self.ps = [Process(target=worker, args=(work_remote, CloudpickleWrapper(env_fn)))
+            for (work_remote, env_fn) in zip(self.work_remotes, env_fns)]
+        for p in self.ps:
+            p.start()
+
+        self.remotes[0].send(('get_spaces', None))
+        self.action_space, self.observation_space = self.remotes[0].recv()
+
+    def step(self, actions):
+        for remote, action in zip(self.remotes, actions):
+            remote.send(('step', action))
+        results = [remote.recv() for remote in self.remotes]
+        obs, rews, dones, infos = zip(*results)
+
+        return np.stack(obs), np.stack(rews), np.stack(dones), infos
+
+    def step_async(self, actions):
+        for remote, action in zip(self.remotes, actions):
+            remote.send(('step', action))
+        self.waiting = True
+
+    def step_wait(self):
+        results = [remote.recv() for remote in self.remotes]
+        self.waiting = False
+        obs, rews, dones, infos = zip(*results)
+        return np.stack(obs), np.stack(rews), np.stack(dones), infos
+
+    def reset(self):
+        for remote in self.remotes:
+            remote.send(('reset', None))
+        return np.stack([remote.recv() for remote in self.remotes])
+
+    def reset_task(self):
+        for remote in self.remotes:
+            remote.send(('reset_task', None))
+        return np.stack([remote.recv() for remote in self.remotes])
+
+    def get_history(self, nsteps):
+        for remote in self.remotes:
+            remote.send(('get_history', nsteps))
+        results = [remote.recv() for remote in self.remotes]
+        obs, acts, dones = zip(*results)
+        obs = np.stack(obs)
+        acts = np.stack(acts)
+        dones = np.stack(dones)
+        return obs, acts, dones
+
+    def recursive_getattr(self, name):
+        for remote in self.remotes:
+            remote.send(('recursive_getattr',name))
+        return [remote.recv() for remote in self.remotes]
+
+    def decrement_starting_point(self, n):
+        for remote in self.remotes:
+            remote.send(('decrement_starting_point', n))
+
+    def close(self):
+        for remote in self.remotes:
+            remote.send(('close', None))
+        for p in self.ps:
+            p.join()
+
+    @property
+    def num_envs(self):
+        return len(self.remotes)
+
 
 class ReshapeObsEnv(gym.ObservationWrapper):
     """
